2e26f39e3e0b1d564fc0c6fe029ad42ff8fb4dee
BalmasovAS <asbalmasov@gmail.com>
2021-08-04 20:34:28 +0500

    new

--- js/index.js
+++ js/index.js
@@ -8,9 +8,9 @@ for (let i = 0; i < offer.length; i++) {
         text[i].style.display = (text[i].style.display === 'block') ? 'none' : 'block';
         arrow[i].style.transform = (text[i].style.display === 'block') ? 'rotate(180deg)' : 'rotate(360deg)';
     });
-} 
+}
 
-/* ----------go top btn---------- */
+/* /* ----------go top btn----------
 var goTopBtn = document.querySelector('.to-main');
 
 window.addEventListener('scroll', trackScroll);
@@ -26,7 +26,7 @@ function trackScroll() {
     if (scrolled < coords) {
         goTopBtn.style.display = 'none'
     }
-}
+} */
 
 /* ------------slaider------------- */
 let slideIndex = 1;
@@ -81,3 +81,80 @@ document.querySelector(".form-subscription").addEventListener("submit", (event) 
 
     form.reset()
 })
+
+/* ----------burger----------- */
+document.querySelector('.js-burger').addEventListener('click', function (event) {
+    const menu = document.querySelector('.nav-menu');
+    const isMenuVisible = menu.classList.contains('dn-mobile');
+
+    /* if (menu.classList.contains('dn-mobile')) {
+        menu.classList.remove('dn-mobile');
+        return;
+    }
+
+    menu.classList.add('dn-mobile'); */
+    document.querySelector('.js-burger').classList.toggle('active');
+    document.body.classList.toggle('lock');
+    menu.classList.toggle('active');
+    menu.classList.toggle('dn-mobile', !isMenuVisible);
+});
+
+const isMobile = {
+    Android: function () {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function () {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function () {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function () {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function () {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function () {
+        return (
+            isMobile.Android() ||
+            isMobile.BlackBerry() ||
+            isMobile.iOS() ||
+            isMobile.Opera() ||
+            isMobile.Windows());
+    }
+};
+
+if (isMobile.any()) {
+    document.body.classList.add('touch');
+} else {
+    document.body.classList.add('pc');
+}
+
+const menuLinks = document.querySelectorAll('.menu-link[data-goto]');
+if (menuLinks.length > 0) {
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener('click', onMenuLinkClick);
+    });
+
+    function onMenuLinkClick(e) {
+        const menuLink = e.target;
+        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('.header').offsetHeight;
+
+            if (document.querySelector('.js-burger').classList.contains('active')){
+                document.querySelector('.js-burger').classList.remove('active');
+                document.body.classList.remove('lock');
+                document.querySelector('.nav-menu').classList.remove('active');
+                document.querySelector('.nav-menu').classList.remove('dn-mobile');
+            }
+
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: "smooth"
+            });
+            e.preventDefault();
+        }
+    }
+}
